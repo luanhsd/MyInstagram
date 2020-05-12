d3cc5599a665c34d61cc3c8a42ae7436b213e018
luanhsd <luan_h.s.d@hotmail.com>
2020-05-11 22:00:38 -0300

    start webapp

---
+++ webapp/src/pages/Feed/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Feed() {
+    return (
+        <dv>
+            <h1>Feed</h1>
+        </dv>
+    )
+}
+
+export default Feed;
---
+++ webapp/src/pages/New/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function New() {
+    return (
+        <dv>
+            <h1>New</h1>
+        </dv>
+    )
+}
+
+export default New;
