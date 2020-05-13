d77f7107897e4858f73f4544277c3ff4057ec388
luanhsd <luan_h.s.d@hotmail.com>
2020-05-13 00:15:57 -0300

    webapp

--- webapp/src/pages/Feed/index.js
+++ webapp/src/pages/Feed/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import './styles.css';
+import io from 'socket.io-client'
+import api from '../../services/api'
+import more from '../../assets/more.svg'
+import like from '../../assets/like.svg'
+import comment from '../../assets/comment.svg'
+import send from '../../assets/send.svg'
 
 function Feed() {
+    const [feed, setFeed] = useState([])
+
+    useEffect(() => {
+        api.get(`posts`)
+            .then(response => setFeed(response.data))
+    }, [])
+
+    useEffect(() => {
+        const socket = io(`http://localhost:3001`)
+        socket.on('post', post => {
+            setFeed([post, ...feed])
+        })
+
+        socket.on('like', liked => {
+            setFeed(feed.map(post => post._id === liked._id ? liked : post))
+        })
+    }, [feed])
+
+    function handleLike(id) {
+        console.log(id)
+        api.post(`posts/${id}/like`)
+    }
+
     return (
-        <dv>
-            <h1>Feed</h1>
-        </dv>
+        <section id="post-list">
+            {feed.map(post => (
+                <article key={post._id}>
+                    <header>
+                        <div className="user-info">
+                            <span>{post.author}</span>
+                            <span className="place">{post.place}</span>
+                        </div>
+                        <img src={more} alt="Mais" />
+                    </header>
+                    <img src={`http://localhost:3001/files/${post.image}`} alt="" />
+
+                    <footer>
+                        <div className="actions">
+                            <button type="button" onClick={() => handleLike(post._id)}>
+                                <img src={like} alt="curtir" />
+                            </button>
+                            <img src={comment} alt="comentar" />
+                            <img src={send} alt="compartilhar" />
+                        </div>
+                        <strong>{post.likes} curtidas</strong>
+                        <p>
+                            {post.description}
+                            <span>{post.hashtags}</span>
+                        </p>
+                    </footer>
+                </article>
+            ))}
+        </section>
     )
 }
 
--- webapp/src/pages/New/index.js
+++ webapp/src/pages/New/index.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
+import api from '../../services/api'
 import './styles.css';
 
-function New() {
+function New({ navigation }) {
+
+    const history = useHistory()
+
+    const [post, setPost] = useState({
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: ''
+    })
+
+    async function handlePost(event) {
+        event.preventDefault()
+        const data = new FormData()
+        data.append('image', post.image)
+        data.append('author', post.author)
+        data.append('place', post.place)
+        data.append('description', post.description)
+        data.append('hashtags', post.hashtags)
+
+        await api.post(`posts`, data)
+        history.push('/')
+    }
+
+
+
     return (
-        <dv>
-            <h1>New</h1>
-        </dv>
+        <form id="new-post" onSubmit={handlePost}>
+            <input type="file"
+                name="image"
+                onChange={e => setPost({ ...post, image: e.target.files[0] })}
+            />
+
+            <input
+                type="text"
+                name="author"
+                value={post.author}
+                onChange={e => setPost({ ...post, author: e.target.value })}
+                placeholder="Autor do post" />
+
+            <input
+                type="text"
+                name="place"
+                value={post.place}
+                onChange={e => setPost({ ...post, place: e.target.value })}
+                placeholder="Local do post" />
+
+            <input
+                type="text"
+                name="description"
+                value={post.description}
+                onChange={e => setPost({ ...post, description: e.target.value })}
+                placeholder="Descrição do post" />
+
+            <input
+                type="text"
+                name="hashtags"
+                value={post.hashtags}
+                onChange={e => setPost({ ...post, hashtags: e.target.value })}
+                placeholder="Hashtags do post" />
+
+            <button type="submit">Enviar</button>
+        </form>
     )
 }
 
